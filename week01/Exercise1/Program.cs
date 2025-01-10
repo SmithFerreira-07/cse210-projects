0f9bdcb426ac6c5a71d85e9249677d27c5a51f21
SmithFerreira-07 <GitFerreira@proton.me>
2025-01-09 22:51:55 -0300

    first change

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Runtime.CompilerServices;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
+        Console.WriteLine("What is your First Name?. ");
+        string firstname = Console.ReadLine();
+        Console.WriteLine("What is your Last Name?. ");
+
     }
+
+    
 }
