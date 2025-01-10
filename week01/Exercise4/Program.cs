7a9047377b805149c7d811e1ecc37bb72a888eb8
SmithFerreira-07 <GitFerreira@proton.me>
2025-01-10 15:49:07 -0300

    added foreach loop to find largestNumber

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -9,21 +9,32 @@ class Program
         List<int> numbers = new List<int>();
         Console.WriteLine("Enter a list of numbers, type 0 when finished.");
         int numberInput = 893812;
+        int largestNumber = 0 ;
         while (true)
         {
             Console.WriteLine("Enter number: ");
             numberInput = int.Parse(Console.ReadLine());
             if (numberInput == 0)
             {
-                Console.WriteLine("BREAKING");
+                Console.WriteLine("Computing...");
                 break;
             }
             numbers.Add(numberInput);
+            foreach (int numeron in numbers)
+            {
+                if (numeron > largestNumber)
+                {
+                    largestNumber = numeron;
+                }
+            }
+
             
         }
         //Console.WriteLine(numbers.Count);
         int totalSum = numbers.Sum();
         double totalAverage = numbers.Average();
         Console.WriteLine($"The sum is: {totalSum}");
+        Console.WriteLine($"The average is: {totalAverage}");
+        Console.WriteLine($"The largest number is: {largestNumber}");
     }
 }
