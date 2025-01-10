f297d60ed8bb1b3c6eb3fa46c5d2123921893018
SmithFerreira-07 <GitFerreira@proton.me>
2025-01-09 23:46:59 -0300

    first commit of exercise 3

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        
+        Console.WriteLine("What is the magic number?. ");
+        string magicString = Console.ReadLine();
+        int magicNumber = int.Parse(magicString);
+        Console.WriteLine("What is your guess? ");
+        string guess = Console.ReadLine();
+        int guessNumber = int.Parse(guess);
+        if (magicNumber > guessNumber)
+        {
+            Console.WriteLine("Higher");
+        }
+        else if (magicNumber < guessNumber)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+
     }
 }
