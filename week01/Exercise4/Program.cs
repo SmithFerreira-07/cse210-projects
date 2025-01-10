f5cf00365fe471b2cd498ab6ce166ebd4d50abc9
SmithFerreira-07 <GitFerreira@proton.me>
2025-01-10 15:26:34 -0300

    first prototype

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        //Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int numberInput = 893812;
+        while (numberInput != 0)
+        {
+            Console.WriteLine("Enter number: ");
+            numberInput = int.Parse(Console.ReadLine());
+            numbers.Add(numberInput);
+        }
     }
 }
