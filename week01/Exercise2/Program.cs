43a7992b978ec206a0170096e884243d8bee524c
SmithFerreira-07 <GitFerreira@proton.me>
2025-01-09 23:28:28 -0300

    fixed some parts of the code to be more Core Requirement compliant, also created a separate variable for the grade letter

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,29 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        
         Console.WriteLine("Hello user what was your grade percentage?. ");
         string grade = Console.ReadLine();
         int gradeInt = int.Parse(grade);
+        string gradeLetter = "";
         if (gradeInt >= 90)
         {
-            Console.WriteLine("Congratulations! you pass with an A");
+            gradeLetter = "A";
         }
         else if (gradeInt >= 80)
         {
-            Console.WriteLine("Congratulations! you pass with a B!");
+            gradeLetter = "B";
         }
         else if (gradeInt >= 70)
         {
-            Console.WriteLine("You passed with a C, you can do better!");
+            gradeLetter = "C";
         }
         else if (gradeInt >= 60)
         {
-            Console.WriteLine("You got a D, you failed!");
+           gradeLetter = "D";
         }
-        else if (gradeInt < 60)
+        else
         {
-            Console.WriteLine("You got a F grade, study harder next time");
+            gradeLetter = "F";
+        }
+
+        Console.WriteLine($"The grade you got was: {gradeLetter}");
+
+        if (gradeInt >= 70)
+        {
+            Console.WriteLine("You passed! Good Job");
+        }
+        else
+        {
+            Console.WriteLine("You failed study harder next time!");
         }
     }
 }
