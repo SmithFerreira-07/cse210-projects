e50cb8ee85066e07e86b9d0c3b9389e80dc9b8e7
SmithFerreira-07 <GitFerreira@proton.me>
2025-01-09 23:13:17 -0300

    adding else ifs

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("Hello user what was your grade percentage?. ");
+        string grade = Console.ReadLine();
+        int gradeInt = int.Parse(grade);
+        if (gradeInt >= 90)
+        {
+            Console.WriteLine("Congratulations! you pass with an A");
+        }
+        else if (gradeInt >= 80)
+        {
+            Console.WriteLine("Congratulations! you pass with a B!");
+        }
     }
 }
