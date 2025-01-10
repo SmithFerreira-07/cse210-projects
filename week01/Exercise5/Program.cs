0ed478286c061c66489922e82140ec1966fcf134
SmithFerreira-07 <GitFerreira@proton.me>
2025-01-10 16:18:28 -0300

    problems with square function and display

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,29 +4,45 @@ class Program
 {
     static void Main(string[] args)
     {
+
+        string name = "";
+        int numberInt = 920392;
         //Console.WriteLine("Hello World! This is the Exercise5 Project.");
         static void DisplayWelcome()
         {
             Console.WriteLine("Welcome to the Program!");
 
         }
-        static string PromptUserName()
+        static string PromptUserName(string name)
         {
             Console.WriteLine("Please enter your name: ");
-            string name = Console.ReadLine();
+            name = Console.ReadLine();
             return name;
         }
 
-        static int PromptUserNumber()
+        static int PromptUserNumber(int numberInt)
         {
             Console.WriteLine("Please enter your favorite number: ");
             string number = Console.ReadLine();
-            int numberInt = int.Parse(number);
+            numberInt = int.Parse(number);
             return numberInt;
         }
 
+        static int SquareNumber(int numberInt)
+        {
+            int square = numberInt * numberInt;
+            return square;
+        }
+        static void DisplayResult(string name, int square)
+        {
+            Console.WriteLine($"{name}, the square of your number is {square}");
+        }
+
         DisplayWelcome();
-        PromptUserName();
-        PromptUserNumber();
+        PromptUserName(name);
+        PromptUserNumber(numberInt);
+        SquareNumber(numberInt);
+        DisplayResult(name, numberInt);
+        
     }
 }
