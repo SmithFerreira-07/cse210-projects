34603b2d9e182229821330a6c8d514d87a23fe94
SmithFerreira-07 <GitFerreira@proton.me>
2025-01-10 16:04:07 -0300

    creating functions

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        //Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        static void DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+
+        }
+        static string PromptUserName()
+        {
+            Console.WriteLine("Please enter your name: ");
+            string name = Console.ReadLine();
+            return name;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.WriteLine("Please enter your favorite number: ");
+            string number = Console.ReadLine();
+            int numberInt = int.Parse(number);
+            return numberInt;
+        }
+
+        DisplayWelcome();
+        PromptUserName();
+        PromptUserNumber();
     }
 }
