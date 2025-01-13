2f9333b6fd5184d0b57896807fd94b247589d129
SmithFerreira-07 <GitFerreira@proton.me>
2025-01-13 18:25:39 -0300

    added DisplayJobDetails method

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -9,8 +9,14 @@ class Program
         Job job2 = new Job();
         job1._jobTitle = "Software Engineer";
         job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
         job2._jobTitle = "Manager";
         job2._company = "Apple";
-        Console.WriteLine($"{job1._company}, {job2._company}");
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+        //Console.WriteLine($"{job1._company}, {job2._company}");
+        job1.DisplayJobDetails();
+        job2.DisplayJobDetails();
     }
 }
