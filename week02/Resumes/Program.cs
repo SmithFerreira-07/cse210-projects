846becc7136555b6be4bd9b5c40d1162a5718863
SmithFerreira-07 <GitFerreira@proton.me>
2025-01-13 12:30:44 -0300

    testing the job class

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        Job job2 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        Console.WriteLine($"{job1._company}, {job2._company}");
     }
 }
