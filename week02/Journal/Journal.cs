a1c12be8495c70818af8dd532b5d344f7c8dc737
SmithFerreira-07 <GitFerreira@proton.me>
2025-01-16 18:44:01 -0300

    finished skeleton for Journal class

--- week02/Journal/Journal.cs
+++ week02/Journal/Journal.cs
@@ -3,4 +3,24 @@ using System;
 public class Journal
 {
     public List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry newEntry)
+    {
+        _entries.Add(newEntry);
+    }
+
+    public void DisplayAll()
+    {
+        
+    }
+
+    public void SaveToFile(string file)
+    {
+
+    }
+
+    public void LoadFromFile(string file)
+    {
+
+    }
 }
