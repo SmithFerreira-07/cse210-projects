18a5e8c27c3ca0f4c2b5ab3aff6b9aa878e8fb0d
SmithFerreira-07 <GitFerreira@proton.me>
2025-01-20 19:28:46 -0300

    step 4 constructors

--- week03/Fractions/Fraction.cs
+++ week03/Fractions/Fraction.cs
@@ -3,4 +3,34 @@ using System;
 public class Fraction{
     private int _top;
     private int _bottom;
+
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int top)
+    {
+        _top = top;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        if (bottom == 0)
+        {
+            Console.WriteLine("The Denominator can't be Zero");
+        }
+
+        _top = top;
+        _bottom = bottom;
+    }
+    
+    public string GetFraction()
+    {
+        return $"{_top}/{_bottom}";
+        
+    }
 }
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Fractions Project.");
+
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine($"Fraction 1: {fraction1.GetFraction()}");
+
+        
+        Fraction fraction2 = new Fraction(5);
+        Console.WriteLine($"Fraction 2: {fraction2.GetFraction()}");
+
+        
+        Fraction fraction3 = new Fraction(5, 7);
+        Console.WriteLine($"Fraction 3: {fraction3.GetFraction()}");
     }
 }
