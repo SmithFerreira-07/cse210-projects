c209a4fe072b8353bd5dbd7ac709228e2894f032
SmithFerreira-07 <GitFerreira@proton.me>
2025-01-22 19:44:25 -0300

    created constructor for range and for single verse in reference.cs

--- week03/ScriptureMemorizer/Reference.cs
+++ week03/ScriptureMemorizer/Reference.cs
@@ -7,11 +7,20 @@ public class Reference
     private int _verse;
     private int? _endVerse;
 
-    public Reference(string book, int chapter, int verse, int? endVerse = null)
+    public Reference(string book, int chapter, int verse)
     {
         _book = book;
         _chapter = chapter;
         _verse = verse;
+        _endVerse = null; 
+    }
+
+    
+    public Reference(string book, int chapter, int startVerse, int endVerse)
+    {
+        _book = book;
+        _chapter = chapter;
+        _verse = startVerse;
         _endVerse = endVerse;
     }
 
--- week03/ScriptureMemorizer/Scripture.cs
+++ week03/ScriptureMemorizer/Scripture.cs
@@ -4,4 +4,34 @@ public class Scripture
 {
     private Reference _reference;
     private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = new List<Word>();
+
+        foreach (var word in text.Split(' '))
+        {
+            _words.Add(new Word(word));
+        }
+
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        var revealedWords = new List<Word>();
+
+        foreach (var word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                revealedWords.Add(word);
+            }
+        }
+
+        var random = new Random();
+        var wordsToHide = new List<Word>();
+
+       
+    }
 }
