54cbaeaddf1d5ceade9d5abab0a5ae181cceca94
SmithFerreira-07 <GitFerreira@proton.me>
2025-01-22 20:59:42 -0300

    exceeding requirements adding scripture shelf

---
+++ week03/ScriptureMemorizer/ScriptureShelf.cs
@@ -0,0 +1,22 @@
+using System;
+
+public class ScriptureShelf
+{
+    private List<Scripture>_scriptures;
+
+    public void AddScripture(Scripture scripture)
+    {
+        _scriptures.Add(scripture);
+    }
+
+    public Scripture GetRandomScripture()
+    {
+        if (_scriptures.Count == 0)
+        {
+            Console.WriteLine("There are no Scriptures");
+        }
+
+        Random random = new Random();
+        return _scriptures[random.Next(_scriptures.Count)];
+    }
+}
