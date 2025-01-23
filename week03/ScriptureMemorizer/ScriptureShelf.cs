17e252155abedb416d4fed41a4df256175436b9d
SmithFerreira-07 <GitFerreira@proton.me>
2025-01-22 21:00:27 -0300

    forgot to add space to the list :(

--- week03/ScriptureMemorizer/ScriptureShelf.cs
+++ week03/ScriptureMemorizer/ScriptureShelf.cs
@@ -2,7 +2,7 @@ using System;
 
 public class ScriptureShelf
 {
-    private List<Scripture>_scriptures;
+    private List<Scripture> _scriptures;
 
     public void AddScripture(Scripture scripture)
     {
