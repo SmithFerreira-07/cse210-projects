63f3e42314f714241db9887381f7bc5a45172a2a
SmithFerreira-07 <GitFerreira@proton.me>
2025-02-03 18:34:29 -0300

    testing writing assignment

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -8,6 +8,10 @@ class Program
         MathAssignment mathAssignment = new MathAssignment("Steve Jobs", "Multiplication", "8.8", "10-20");
         Console.WriteLine(mathAssignment.GetSummary());
         Console.WriteLine(mathAssignment.GetHomeworkList());
+        Console.WriteLine();
+        WritingAssignment writingAssignment = new WritingAssignment("Arthur Nelson", "European History", "The Causes of World War II");
+        Console.WriteLine(writingAssignment.GetSummary());
+        Console.WriteLine(writingAssignment.GetWritingInformation());
         
     }
 }
