feb80f6c7c23729bebe33b5624e5776b1e5c6a56
SmithFerreira-07 <GitFerreira@proton.me>
2025-02-03 18:21:25 -0300

    testing mathassignment, making writingassignment

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -5,5 +5,9 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Homework Project.");
+        MathAssignment mathAssignment = new MathAssignment("Steve Jobs", "Multiplication", "8.8", "10-20");
+        Console.WriteLine(mathAssignment.GetSummary());
+        Console.WriteLine(mathAssignment.GetHomeworkList());
+        
     }
 }
--- week05/Homework/WritingAssignment.cs
+++ week05/Homework/WritingAssignment.cs
@@ -1,7 +1,19 @@
 using System;
 
-public class WritingAssignment
+public class WritingAssignment : Assignment
 {
     private string _title;
+
+    public WritingAssignment(string studentName, string topic, string title) : base(studentName, topic)
+    {
+        _title = title;
+    }
+
+    public string GetWritingInformation()
+    {
+        return $"{_title} by {GetStudentName()}";
+    }
+
+
 
 }
