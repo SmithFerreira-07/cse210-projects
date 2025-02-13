86be9f91ac8db21adb4a1b424c3d297af568d6b1
SmithFerreira-07 <GitFerreira@proton.me>
2025-02-13 16:28:27 -0300

    goal constructor

---
+++ week06/EternalQuest/Goal.cs
@@ -0,0 +1,15 @@
+using System;
+
+public abstract class Goal
+{
+    private string _shortName;
+    private string _description;
+    private int _points;
+
+    public Goal(string name, string description, int points)
+    {
+        _shortName = name;
+        _description = description;
+        _points = points;
+    }
+}
