9fac4ad7434e428807b36c259bd81a1c55d14004
SmithFerreira-07 <GitFerreira@proton.me>
2025-02-13 15:38:44 -0300

    fixing faulty objects

--- week06/Shapes/Rectangle.cs
+++ week06/Shapes/Rectangle.cs
@@ -5,7 +5,7 @@ public class Rectangle : Shape
     private double _length;
     private double _width;
 
-    public Rectangle(double length, double width) : base(color)
+    public Rectangle(string color, double length, double width) : base(color)
     {
         _length = length;
         _width = width;
--- week06/Shapes/Shape.cs
+++ week06/Shapes/Shape.cs
@@ -4,6 +4,11 @@ public abstract class Shape
 {
     private string _color;
 
+    public Shape(string color)
+    {
+        _color = color;
+    }
+
     public void SetColor(string color)
     {
         _color = color;
