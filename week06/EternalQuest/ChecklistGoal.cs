dc60a641d59a02989a86fbd374fbc3b2f1e0ba4b
SmithFerreira-07 <GitFerreira@proton.me>
2025-02-13 17:21:37 -0300

    Checklist goal constructor

---
+++ week06/EternalQuest/ChecklistGoal.cs
@@ -0,0 +1,17 @@
+using System;
+
+public class ChecklistGoal : Goal
+{
+    private int _amountCompleted;
+
+    private int _target;
+
+    private int _bonus;
+
+    public ChecklistGoal(string name, string description, int points, int target, int bonus) : base(name, description, points)
+    {
+        _amountCompleted = 0;
+        _target = target;
+        _bonus = bonus;
+    }
+}
