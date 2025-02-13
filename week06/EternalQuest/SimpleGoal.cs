e20e3d33b07b3f826bbce4c41da45fb34693ac50
SmithFerreira-07 <GitFerreira@proton.me>
2025-02-13 16:38:26 -0300

    simplegoal constructor

---
+++ week06/EternalQuest/SimpleGoal.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+public class SimpleGoal : Goal
+{
+    private bool _isComplete;
+
+    public SimpleGoal(string name, string description, int points) : base(name, description, points)
+    {
+        _isComplete = false;
+    }
+}
