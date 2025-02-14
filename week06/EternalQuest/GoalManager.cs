c92821bc178dc1a7330c1899a4931af2a6c7942e
SmithFerreira-07 <GitFerreira@proton.me>
2025-02-14 14:56:05 -0300

    menu body goalmanager

---
+++ week06/EternalQuest/GoalManager.cs
@@ -0,0 +1,15 @@
+using System;
+
+public class GoalManager
+{
+    private List<Goal> _goals = new List<Goal>();
+    private int _score = 0;
+
+    public void Start()
+    {
+        while(true)
+        {
+            Console.WriteLine("This is the menu!");
+        }
+    }
+}
