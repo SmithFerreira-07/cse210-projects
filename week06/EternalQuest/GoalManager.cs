b18104acc708578986c8f7676dd5d081c986d6e2
SmithFerreira-07 <GitFerreira@proton.me>
2025-02-14 15:44:54 -0300

    Menu and opening switch case

--- week06/EternalQuest/GoalManager.cs
+++ week06/EternalQuest/GoalManager.cs
@@ -10,6 +10,15 @@ public class GoalManager
         while(true)
         {
             Console.WriteLine("This is the menu!");
+            Console.WriteLine("1. Player Status");
+            Console.WriteLine("2. List your Goals");
+            Console.WriteLine("3. List your Goal Objectives");
+            Console.WriteLine("4. Create your Goal");
+            Console.WriteLine("5. Record an Event");
+            Console.WriteLine("6. Save your Goals");
+            Console.WriteLine("7. Load your Goals");
+            Console.WriteLine("8. Close Program");
+
         }
     }
 }
