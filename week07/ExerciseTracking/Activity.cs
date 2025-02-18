be979098613cda9b4eaed0faed78bc2fe0838e11
SmithFerreira-07 <GitFerreira@proton.me>
2025-02-18 16:41:42 -0300

    added the abstract methods and getsummary, also finished cycling derived class

--- week07/ExerciseTracking/Activity.cs
+++ week07/ExerciseTracking/Activity.cs
@@ -1,14 +1,14 @@
 using System;
 using System.Runtime.CompilerServices;
 
-public class Activity
+public abstract class Activity
 {
     private DateTime _date;
     private int _minutes;
 
-    public Activity(DateTime date, int minutes)
+    public Activity(int minutes)
     {
-        _date = date;
+        _date = DateTime.Now;
         _minutes = minutes;
     }
 
@@ -22,4 +22,12 @@ public class Activity
         return _minutes;
     }
     
+    public abstract double GetDistance();
+    public abstract double GetSpeed();
+    public abstract double GetPace();
+
+    public string GetSummary()
+    {
+        return $"{_date:dd MMM yyyy} {GetType().Name} ({_minutes} min) - Distance: {GetDistance()} miles, Speed {GetSpeed():F1} mph, Pace: {GetPace():F1} min per mile";
+    }
 }
--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -5,5 +5,16 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+
+        Activity cycling = new Cycling(45, 15.0);
+
+        List<Activity> activites = new List<Activity>();
+
+        activites.Append(cycling);
+
+        foreach (var activity in activites)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
     }
 }
